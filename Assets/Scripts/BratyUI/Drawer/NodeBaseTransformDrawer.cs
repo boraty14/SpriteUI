f423d11d3aef0a6f1aa243ddca091d52b97b4526
braty <brataysen@gmail.com>
2024-05-02 08:55:37 +0300

    added ui toolkit drawer

---
+++ Assets/Scripts/BratyUI/Drawer/NodeBaseTransformDrawer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace BratyUI.Drawer
+{
+    [CustomEditor(typeof(Transform))]
+    [CanEditMultipleObjects]
+    public class NodeBaseTransformDrawer : Editor
+    {
+        public override VisualElement CreateInspectorGUI()
+        {
+            VisualElement root = new VisualElement();
+
+            if (((Transform)serializedObject.targetObject).gameObject.TryGetComponent(out NodeBase node))
+            {
+                // Add your custom UI elements here
+                // ...
+
+                return root;
+            }
+
+            return base.CreateInspectorGUI();
+            // If the condition is not met, return the default inspector
+        }
+    }
+}
