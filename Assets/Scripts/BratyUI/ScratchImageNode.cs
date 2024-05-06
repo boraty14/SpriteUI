74d486e395df5501c87e3bc3a67d8d3d2fdf552a
braty <brataysen@gmail.com>
2024-05-06 06:59:53 +0300

    size offset fix

--- Assets/Scripts/BratyUI/ScratchImageNode.cs
+++ Assets/Scripts/BratyUI/ScratchImageNode.cs
@@ -9,7 +9,7 @@ namespace BratyUI
     public class ScratchImageNode : NodeBase
     {
         [SerializeField] private Vector2 _position;
-        [SerializeField] private Vector2 _size;
+        [SerializeField] private Vector2 _sizeOffset;
         [SerializeField] private Vector2 _minAnchor = Vector2.zero;
         [SerializeField] private Vector2 _maxAnchor = Vector2.one;
         [SerializeField] private SpriteRenderer _spriteRenderer;
@@ -23,15 +23,15 @@ namespace BratyUI
             var rightTop = NodeCanvas.GetAnchorPosition(_maxAnchor);
 
             var size = (Vector2)(rightTop - leftBottom);
-            size += _size;
+            //size += _size;
             _spriteRenderer.drawMode = SpriteDrawMode.Sliced;
-            _spriteRenderer.size = size;
+            _spriteRenderer.size = size + _sizeOffset;
 
             var position = (Vector2)leftBottom + size * 0.5f + _position;
             transform.localPosition = new Vector3(position.x, position.y, leftBottom.z);
             transform.localScale = Vector3.one;
             
-            _collider.size = size;
+            _collider.size = size + _sizeOffset;
         }
     }
 }
